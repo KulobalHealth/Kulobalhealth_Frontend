058adfda115ea0f1f4cad01b2f740fe0caf6e97f
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-05-10 21:04:38 +0000

    chore: comment imports

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,14 +1,14 @@
-import Approach from "@/components/approach";
-import { Hero } from "@/components/hero";
-import ValuedInvestors from "@/components/investors";
-import Impact from "@/components/impact";
-import ImpactCashFlow from "@/components/impactCashflow";
-import ImpactCounterfeits from "@/components/impactCounterfeits";
-import ImpactStats from "@/components/impactStats";
-import ImpactMap from "@/components/impactMap";
-import DawaMkononiApp from "@/components/dawaMkononiApp";
-import Grants from "@/components/grants";
-import { AnimationWrapper } from "@/components/ui/animation-wrapper";
+// import Approach from "@/components/approach";
+// import { Hero } from "@/components/hero";
+// import ValuedInvestors from "@/components/investors";
+// import Impact from "@/components/impact";
+// import ImpactCashFlow from "@/components/impactCashflow";
+// import ImpactCounterfeits from "@/components/impactCounterfeits";
+// import ImpactStats from "@/components/impactStats";
+// import ImpactMap from "@/components/impactMap";
+// import DawaMkononiApp from "@/components/dawaMkononiApp";
+// import Grants from "@/components/grants";
+// import { AnimationWrapper } from "@/components/ui/animation-wrapper";
 import { redirect } from "next/navigation";
 
 export default function LandingPage() {
