1f5d7cf66e4b80fa6f01671906d2a74316accbe0
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-05-10 22:21:39 +0000

    refactor(MKP-2): simplify navigation links structure and improve active link styling

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,26 +4,25 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { User } from "./user";
-import { Bell, Moon, ShoppingCart } from "lucide-react";
+import { Bell, ShoppingCart } from "lucide-react";
 import { useMarketplaceStore } from "@/lib/store";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { ModeToggle } from "./ui/mode-toggle";
 
-const navigationLinks = {
-  primary: [
-    { name: "Home", href: "/" },
-    { name: "Marketplace", href: "/marketplace" },
-    { name: "Detection", href: "/detection" },
-  ],
-  company: [
-    { name: "About", href: "/about" },
-    { name: "Team", href: "/team" },
-    { name: "Contact", href: "/contact" },
-  ],
-};
+const navigationLinks = [
+  { name: "Home", href: "/" },
+  { name: "Marketplace", href: "/marketplace" },
+  { name: "Detection", href: "/detection" },
+  { name: "Company", href: "/company" },
+];
 
 export function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
   const cart = useMarketplaceStore((state) => state.cart);
+  const pathname = usePathname();
+  const isActive = (href: string) => pathname === href;
 
   useEffect(() => {
     const handleScroll = () => {
@@ -37,7 +36,7 @@ export function Navbar() {
     <nav
       className={
         isScrolled
-          ? "fixed top-0 left-0 right-0 z-50 bg-white shadow-lg transition duration-300"
+          ? "fixed top-0 left-0 right-0 z-50 bg-white dark:bg-background shadow-lg transition duration-300"
           : "transition duration-300"
       }
     >
@@ -55,26 +54,35 @@ export function Navbar() {
         </div>
 
         <div className="items-center hidden space-x-8 md:flex">
-          {navigationLinks.primary.map((item) => (
+          {navigationLinks.map((item) => (
             <Link
               key={item.name}
               href={item.href}
-              className="transition-colors duration-300 hover:text-emerald-500"
+              className={clsx(
+                "transition-colors duration-300 hover:text-primary-700",
+                {
+                  "text-primary-600": isActive(item.href),
+                  "text-neutral-800": !isActive(item.href),
+                }
+              )}
             >
               {item.name}
             </Link>
           ))}
           <div className="relative group">
-            <button className="transition-colors duration-300 hover:text-emerald-500">
-              Company
-            </button>
             <div className="absolute hidden group-hover:block pt-2 w-48">
               <div className="bg-white shadow-lg rounded-md py-2">
-                {navigationLinks.company.map((item) => (
+                {navigationLinks.map((item) => (
                   <Link
                     key={item.name}
                     href={item.href}
-                    className="block px-4 py-2 hover:bg-emerald-50 transition-colors duration-300"
+                    className={clsx(
+                      "block px-4 py-2 hover:bg-emerald-50 transition-colors duration-300",
+                      {
+                        "text-primary-600": isActive(item.href),
+                        "text-neutral-800": !isActive(item.href),
+                      }
+                    )}
                   >
                     {item.name}
                   </Link>
@@ -83,7 +91,7 @@ export function Navbar() {
             </div>
           </div>
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="flex text-neutral-800 items-center space-x-4">
           <User
             user={{
               name: "Test User",
@@ -93,7 +101,7 @@ export function Navbar() {
           />
           <div className="relative">
             <Link href="/marketplace/cart">
-              <ShoppingCart className="cursor-pointer hover:text-emerald-500 transition-colors" />
+              <ShoppingCart className="cursor-pointer text-neur hover:text-primary-700 transition-colors" />
               {cart.length > 0 && (
                 <span className="absolute -top-2 -right-2 bg-emerald-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
                   {cart.length}
@@ -102,7 +110,7 @@ export function Navbar() {
             </Link>
           </div>
           <Bell />
-          <Moon />
+          <ModeToggle />
         </div>
 
         <button className="p-2 md:hidden" onClick={() => setIsOpen(!isOpen)}>
@@ -111,59 +119,6 @@ export function Navbar() {
           <div className="w-6 h-0.5 bg-gray-600"></div>
         </button>
       </div>
-
-      {/* {isOpen && (
-        <div className="fixed inset-0 z-50 md:hidden">
-          <div
-            className="fixed inset-0 bg-black/50"
-            onClick={() => setIsOpen(false)}
-          ></div>
-          <div className="fixed top-0 bottom-0 left-0 w-64 bg-white shadow-lg">
-            <div className="p-4 flex justify-between items-center border-b">
-              <Link href="/" className="text-2xl font-bold text-emerald-500">
-                <Image
-                  src="/logo.webp"
-                  alt="KulobalHealth"
-                  width={180}
-                  height={180}
-                  className="w-12 h-auto"
-                />
-              </Link>
-              <button onClick={() => setIsOpen(false)} className="p-2">
-                ✕
-              </button>
-            </div>
-            <div className="px-4 py-6 space-y-4">
-              {navigationLinks.primary.map((item) => (
-                <Link
-                  key={item.name}
-                  href={item.href}
-                  className="block transition-colors duration-300 hover:text-emerald-500"
-                >
-                  {item.name}
-                </Link>
-              ))}
-              <div className="py-2 border-t">
-                {navigationLinks.company.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className="block py-2 transition-colors duration-300 hover:text-emerald-500"
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
-              <Link
-                href="/login"
-                className="block px-4 py-2 bg-emerald-500 text-white rounded transition duration-300 hover:bg-emerald-600"
-              >
-                Login
-              </Link>
-            </div>
-          </div>
-        </div>
-      )} */}
     </nav>
   );
 }
