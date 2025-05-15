ab56bd4e2185d6e889b3e608ee02fa34bc44640e
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-05-15 18:27:39 +0000

    chore: restore homepage

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,64 +1,56 @@
-// import Approach from "@/components/approach";
-// import { Hero } from "@/components/hero";
-// import ValuedInvestors from "@/components/investors";
-// import Impact from "@/components/impact";
-// import ImpactCashFlow from "@/components/impactCashflow";
-// import ImpactCounterfeits from "@/components/impactCounterfeits";
-// import ImpactStats from "@/components/impactStats";
-// import ImpactMap from "@/components/impactMap";
-// import DawaMkononiApp from "@/components/dawaMkononiApp";
-// import Grants from "@/components/grants";
-// import { AnimationWrapper } from "@/components/ui/animation-wrapper";
-import { redirect } from "next/navigation";
+import Approach from "@/components/approach";
+import { Hero } from "@/components/hero";
+import ValuedInvestors from "@/components/investors";
+import Impact from "@/components/impact";
+import ImpactCashFlow from "@/components/impactCashflow";
+import ImpactCounterfeits from "@/components/impactCounterfeits";
+import ImpactStats from "@/components/impactStats";
+import ImpactMap from "@/components/impactMap";
+import DawaMkononiApp from "@/components/dawaMkononiApp";
+import Grants from "@/components/grants";
+import { AnimationWrapper } from "@/components/ui/animation-wrapper";
 
 export default function LandingPage() {
-  redirect("/login");
-
   return (
-    // <div className="font-urbanist">
-    //   <AnimationWrapper>
-    //     <Hero />
-    //   </AnimationWrapper>
-    //   <AnimationWrapper>
-    //     <ValuedInvestors />
-    //   </AnimationWrapper>
+    <div>
+      <AnimationWrapper>
+        <Hero />
+      </AnimationWrapper>
+      <AnimationWrapper>
+        <ValuedInvestors />
+      </AnimationWrapper>
 
-    //   <AnimationWrapper>
-    //     <Approach />
-    //   </AnimationWrapper>
+      <AnimationWrapper>
+        <Approach />
+      </AnimationWrapper>
 
-    //   <AnimationWrapper>
-    //     <Impact />
-    //   </AnimationWrapper>
+      <AnimationWrapper>
+        <Impact />
+      </AnimationWrapper>
 
-    //   <AnimationWrapper>
-    //     <ImpactCashFlow />
-    //   </AnimationWrapper>
+      <AnimationWrapper>
+        <ImpactCashFlow />
+      </AnimationWrapper>
 
-    //   <AnimationWrapper>
-    //     <ImpactCounterfeits />
-    //   </AnimationWrapper>
+      <AnimationWrapper>
+        <ImpactCounterfeits />
+      </AnimationWrapper>
 
-    //   <AnimationWrapper>
-    //     <ImpactStats />
-    //   </AnimationWrapper>
+      <AnimationWrapper>
+        <ImpactStats />
+      </AnimationWrapper>
 
-    //   <AnimationWrapper>
-    //     <ImpactMap />
-    //   </AnimationWrapper>
+      <AnimationWrapper>
+        <ImpactMap />
+      </AnimationWrapper>
 
-    //   <AnimationWrapper>
-    //     <DawaMkononiApp />
-    //   </AnimationWrapper>
+      <AnimationWrapper>
+        <DawaMkononiApp />
+      </AnimationWrapper>
 
-    //   <AnimationWrapper>
-    //     <Grants />
-    //   </AnimationWrapper>
-    // </div>
-    // page under construction
-
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1>Page Under Construction</h1>
+      <AnimationWrapper>
+        <Grants />
+      </AnimationWrapper>
     </div>
   );
 }
