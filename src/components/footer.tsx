87d8c0185338c94c062f74658dcab0ed62cc5ca4
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-05-16 18:17:47 +0000

    feat(MKP-4): Revamp Footer component with new layout, links, and social media icons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,99 +1,143 @@
+import { Facebook, Instagram, Twitter } from "lucide-react";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { Icons } from "./ui/icons";
-import { ChevronDown, ClipboardMinus, MessageCircleMore } from "lucide-react";
 
-export function Footer() {
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+const usefulLinks = [
+  { href: "#", text: "Privacy Policy" },
+  { href: "#", text: "Terms of Service" },
+  { href: "#", text: "Cookies Settings" },
+];
+
+const productsAndServices = [
+  { href: "#", text: "Kulobal.com" },
+  { href: "#", text: "Marketplace" },
+  { href: "#", text: "Detection" },
+];
+
+const helpLinks = [
+  { href: "#", text: "Help Center" },
+  { href: "#", text: "FAQs" },
+  { href: "#", text: "Partner Services" },
+];
+
+export default function Footer() {
   return (
-    <footer className="py-16 font-urbanist bg-primary-foreground">
-      <div className="container px-6 mx-auto">
-        <div className="grid gap-y-8 gap-x-12 lg:grid-cols-12">
-          <div className="lg:col-span-4">
-            <div>
-              <h3 className="mb-4 text-2xl font-semibold">
-                Dawa Mkononi App is available in your favourite store
-              </h3>
-              <div className="flex flex-wrap gap-4">
-                <Button
-                  variant="outline"
-                  className="p-6 text-white bg-black rounded-full"
-                >
-                  <Icons.Playstore className="w-6 h-6" />
-                  Play Store
-                </Button>
-                <Button
-                  variant="outline"
-                  className="p-6 text-white bg-black rounded-full"
-                >
-                  <Icons.Appstore className="w-6 h-6" />
-                  App Store
-                </Button>
-              </div>
-            </div>
+    <footer className="border-t py-[111px] px-8 md:px-16">
+      <div className="mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-8">
+          <div className="flex flex-col gap-3.5">
+            <Link href="/" className="text-2xl font-bold text-primary-500">
+              <Image
+                src="/logo.webp"
+                alt="KulobalHealth"
+                width={180}
+                height={180}
+                className="transition-transform duration-300 hover:brightness-110"
+              />
+            </Link>
+            <p className="text-sm text-gray-700 dark:text-white">
+              Enhance your pharmacy services and improve customer satisfaction
+              with these innovative tools available on Kulobal Health.
+            </p>
           </div>
 
-          <div className="lg:col-span-4">
-            <div>
-              <h3 className="mb-4 text-2xl font-semibold">
-                Transforming Health System through Data-driven Innovation
-              </h3>
-              <Button variant="outline" className="p-6 bg-white rounded-full">
-                <span className="flex items-center gap-2">
-                  English
-                  <ChevronDown />
-                </span>
+          <div>
+            <h3 className="font-medium mb-4">Useful links</h3>
+            <ul className="space-y-2">
+              {usefulLinks.map((link, index) => (
+                <li key={index}>
+                  <Link
+                    href={link.href}
+                    className="text-sm underline dark:text-white text-gray-700 hover:text-primary-500"
+                  >
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h3 className="font-medium mb-4">Products & Services</h3>
+            <ul className="space-y-2">
+              {productsAndServices.map((link, index) => (
+                <li key={index}>
+                  <Link
+                    href={link.href}
+                    className="text-sm text-gray-700 dark:text-white hover:text-primary-500"
+                  >
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h3 className="font-medium mb-4">Help</h3>
+            <ul className="space-y-2">
+              {helpLinks.map((link, index) => (
+                <li key={index}>
+                  <Link
+                    href={link.href}
+                    className="text-sm text-gray-700 dark:text-white hover:text-primary-500"
+                  >
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h3 className="font-medium">Subscribe to our newsletter</h3>
+            <p className="text-sm text-gray-700 dark:text-white">
+              Join our newsletter to stay up to date on features and releases.
+            </p>
+            <div className="flex flex-col gap-4">
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="flex-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500"
+              />
+              <Button className="w-[178px] rounded-xl h-12 font-bold">
+                Subscribe
               </Button>
-            </div>
-          </div>
-
-          <div className="lg:col-span-2">
-            <div>
-              <h3 className="mb-4 font-semibold">
-                <span className="flex items-center gap-2">
-                  <ClipboardMinus />
-                  Explore
-                </span>
-              </h3>
-              <div className="space-y-2">
-                <Link href="#" className="block  hover:text-emerald-500">
-                  About Us
-                </Link>
-                <Link href="#" className="block  hover:text-emerald-500">
-                  Our Contacts
-                </Link>
-                <Link href="#" className="block hover:text-emerald-500">
-                  Privacy Policy
-                </Link>
-              </div>
-            </div>
-          </div>
-
-          <div className="lg:col-span-2">
-            <div>
-              <h3 className="mb-4 font-semibold">
-                <span className="flex items-center gap-2">
-                  <MessageCircleMore />
-                  Socials
-                </span>
-              </h3>
-              <div className="space-y-2">
-                <Link href="#" className="block hover:text-emerald-500">
-                  LinkedIn
-                </Link>
-                <Link href="#" className="block  hover:text-emerald-500">
-                  Twitter
-                </Link>
-                <Link href="#" className="block  hover:text-emerald-500">
-                  Instagram
-                </Link>
-              </div>
             </div>
           </div>
         </div>
 
-        <div className="pt-8 mt-8 text-center border-t border-gray-200">
-          <p>© 2025 DMRX Pharmacy</p>
-          <p>All Rights Reserved</p>
+        <div className="border-t border-gray-200 my-8"></div>
+
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <p className="text-sm text-gray-700 dark:text-white">
+            © 2025 KulobalHealth. All rights reserved.
+          </p>
+          <div className="flex space-x-4 mt-4 md:mt-0">
+            <Link
+              href="#"
+              className="text-gray-700 dark:text-white hover:text-primary-500"
+            >
+              <Facebook size={20} />
+              <span className="sr-only">Facebook</span>
+            </Link>
+            <Link
+              href="#"
+              className="text-gray-700 dark:text-white hover:text-primary-500"
+            >
+              <Instagram size={20} />
+              <span className="sr-only">Instagram</span>
+            </Link>
+            <Link
+              href="#"
+              className="text-gray-700 dark:text-white hover:text-primary-500"
+            >
+              <Twitter size={20} />
+              <span className="sr-only">Twitter</span>
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
