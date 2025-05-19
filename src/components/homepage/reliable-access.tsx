a2edcef0177c0991522283e806e625c80ed341bc
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-05-19 11:52:03 +0000

    feat(MKP-6): add ReliableAccess component to showcase rapid test kit availability

---
+++ src/components/homepage/reliable-access.tsx
@@ -0,0 +1,41 @@
+import reliableAccessImg from "@/../public/allKits.webp";
+import Image from "next/image";
+
+export default function ReliableAccess() {
+  return (
+    <section className="px-4">
+      <div className="container py-5 mx-auto md:py-1">
+        <div className="grid items-center grid-cols-1 gap-10 mb-10 lg:grid-cols-2 lg:gap-0">
+          <div className="relative">
+            <div className="p-0 lg:p-11">
+              <figure
+                className="relative rounded"
+                style={{ width: "100%", height: "0", paddingBottom: "95.25%" }}
+              >
+                <Image
+                  src={reliableAccessImg}
+                  alt="Reliable Access"
+                  fill
+                  objectFit="cover"
+                  className="rounded"
+                />
+              </figure>
+            </div>
+          </div>
+          <div className="lg:max-w-xl container mx-auto">
+            <h2 className="mb-4 text-4xl font-semibold text-primary-600">
+              Reliable Access to RDT&apos;s
+            </h2>
+            <div className="mb-6">
+              <p className="mb-0">
+                Get high-quality rapid test kits for your pharmacy with flexible
+                payment options. Order online no need to call suppliers—and
+                receive delivery within 48 hours.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
