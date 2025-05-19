243f7bb2f603770c9cc68f47c67f3f9ab783c3fa
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-05-19 11:51:35 +0000

    feat(MKP-6): add Hero component for enhanced user engagement on the homepage

---
+++ src/components/homepage/hero.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { motion } from "motion/react";
+import Link from "next/link";
+
+export function Hero() {
+  return (
+    <div className="container py-8 mx-auto min-h-[500px] md:h-[700px] rounded-md flex flex-col items-center justify-center">
+      <div className="w-full max-w-2xl text-center flex flex-col items-center justify-center">
+        <motion.h1
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="mb-4 text-3xl font-bold dark:text-white leading-tight text-gray-800 font-urbanist md:mb-6 md:text-5xl"
+        >
+          Advancing Ghana&apos;s{" "}
+          <span className="text-[#03C486]">Pharmacy Operations</span>
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="mb-6 text-base md:text-lg md:mb-8"
+        >
+          Your One-Stop Marketplace for Rapid Test Kits, Medical Devices, and
+          Essential Supplies
+        </motion.p>
+        <Link href="/marketplace">
+          <Button className="px-6 py-2 bg-[#03C486] rounded-full md:px-8 md:py-5 hover:cursor-pointer text-white hover:bg-primary hover:text-underline">
+            Explore the Marketplace.
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
