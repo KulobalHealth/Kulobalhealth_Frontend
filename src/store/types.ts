08e8efa3be4c042231d3b83a7d63b3fb4237ac6c
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-05-29 13:13:38 +0000

    chore: fix types

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,20 +1,21 @@
 export interface User {
-    firstName: string;
-    lastName: string;
-    email: string;
-    phoneNumber: string;
-    pharmacy: string;
-    licenceNumber: string;
-    type: string;
-    branch: number;
-    region: string;
-    district: string;
-    city: string;
-    gpsCode: string;
-    pharmacyBio: string;
-    password: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  pharmacy: string;
+  licenceNumber: string;
+  type: string;
+  branch: number;
+  region: string;
+  district: string;
+  city: string;
+  gpsCode: string;
+  pharmacyBio: string;
+  password: string;
+  role: string;
+  pharmacyName: string;
+  pharmacyLicenseNumber: string;
+  location: string;
+  totalBranches: string;
 }
-
-
-
-
