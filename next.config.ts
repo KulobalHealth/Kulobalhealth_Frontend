891852336fc087b3127a2b66184eaab407f701e1
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-06-09 16:02:22 +0000

    chore: add dummy fallback for api url

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,14 @@
 import type { NextConfig } from "next";
 
-
-const apiUrl = process.env.NEXT_PUBLIC_API_URL
-if (!apiUrl) {
-  throw new Error("NEXT_PUBLIC_API_URL is not defined in the environment variables.");
-}
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
 
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
     return [
       {
-        source: '/pharmacy/:path*',
-        destination: apiUrl
+        source: "/pharmacy/:path*",
+        destination: apiUrl,
       },
     ];
   },
