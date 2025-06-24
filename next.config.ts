3bea8409592ba6099e8a14dde47fa8a7c3d09997
Pius Prince <45078705+piusprince@users.noreply.github.com>
2025-06-24 13:57:52 +0000

    feat: add biome and ultracite

--- next.config.ts
+++ next.config.ts
@@ -1,39 +1,17 @@
 import type { NextConfig } from "next";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-const apiUrl = process.env.NEXT_PUBLIC_API_URL
-if (!apiUrl) {
-  throw new Error("NEXT_PUBLIC_API_URL is not defined in the environment variables.");
-}
-=======
-const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
->>>>>>> origin
-=======
-const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
->>>>>>> b9e08a776465fb0936ab0b90e9164904d87c5505
+const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001";
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ["images.unsplash.com"],
   },
   async rewrites() {
     return [
       {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        source: '/pharmacy/:path*',
-        destination: apiUrl
-=======
         source: "/pharmacy/:path*",
         destination: apiUrl,
->>>>>>> origin
-=======
-        source: "/pharmacy/:path*",
-        destination: apiUrl,
->>>>>>> b9e08a776465fb0936ab0b90e9164904d87c5505
       },
     ];
   },
